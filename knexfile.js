f4d3c7773f34c0558538deb5c631bbb7aead084c
Scott Van Gilder <svangilder@gmail.com>
2016-10-10 11:29:59 -0600

    take two - heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -9,9 +9,9 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/zenlist_test'
-  }
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
-  },
+  }
 };
