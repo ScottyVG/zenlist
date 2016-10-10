0a4b33fd3671c06bf54def79387d9a9974ff5da7
Scott Van Gilder <svangilder@gmail.com>
2016-10-10 11:36:32 -0600

    add users migration file

---
+++ migrations/20161010113449_users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('firstName', 255).notNullable().defaultTo('');
+    table.string('lastName', 255).notNullable().defaultTo('');
+    table.string('email', 255).notNullable().defaultTo('');
+    table.specificType('hashed_password', 'character(60)').notNullable();
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+};
