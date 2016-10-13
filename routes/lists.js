505cd152d336b76c5101ede661c2e88aaa1e7ade
Scott Van Gilder <svangilder@gmail.com>
2016-10-12 18:18:19 -0600

    working master

--- routes/lists.js
+++ routes/lists.js
@@ -5,25 +5,20 @@ var router = express.Router();
 var knex = require('../db/knex');
 var User = require('../middleware/auth');
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//
-// });
-
-router.get('/:id',User.isLoggedIn,function(req,res,next){
 
 
-router.get('/',User.isLoggedIn,function(req,res,next){
+router.get('/:id',User.isLoggedIn, function(req, res, next) {
 
 })
 
-router.post('/',User.isLoggedIn,function(req, res, next){
-  User.createLists(req,res);
+router.post('/',User.isLoggedIn, function(req, res, next) {
+  User.createLists(req, res);
 })
 
 
-router.post('/edit',User.isLoggedIn,function(req,res,next){
-  console.log('edit post',req.body);
-  User.editLists(req,res);
+router.post('/edit',User.isLoggedIn, function(req, res, next) {
+  console.log('edit post', req.body);
+  User.editLists(req, res);
 })
 
 
